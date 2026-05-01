5b19f95cf45d97dced37fb907c8f3cab76e0bbdd
jjcoding-source <jebinjacob62@gmail.com>
2026-05-01 13:28:29 +0530

    Add property API service for CRUD and saved properties handling

--- src/api/propertyApi.js
+++ src/api/propertyApi.js
@@ -1,7 +1,10 @@
 import api from './axiosInstance'
 
-export const getProperties    = (params) => api.get('/properties', { params })
-export const getPropertyById  = (id)     => api.get(`/properties/${id}`)
-export const createProperty   = (data)   => api.post('/properties', data)
-export const updateProperty   = (id, data) => api.put(`/properties/${id}`, data)
-export const deleteProperty   = (id)     => api.delete(`/properties/${id}`)
+export const getProperties   = (params) => api.get('/properties',              { params })
+export const getPropertyById = (id)      => api.get(`/properties/${id}`)
+export const createProperty  = (data)    => api.post('/properties',             data)
+export const updateProperty  = (id, data)=> api.put(`/properties/${id}`,       data)
+export const deleteProperty  = (id)      => api.delete(`/properties/${id}`)
+export const getSavedProperties = ()     => api.get('/properties/saved')
+export const toggleSaveProperty = (id)   => api.post(`/properties/${id}/save`)
+export const unsaveProperty     = (id)   => api.delete(`/properties/${id}/save`)
